15102356eb81cf48eb895e6e86d729a1535d1231
Afonso Amorim <afonso_sa@hotmail.com>
2018-04-26 23:41:39 -0300

    [Improvement] - Holiday list example

--- src/components/internal/holiday/index.vue
+++ src/components/internal/holiday/index.vue
@@ -1,33 +1,78 @@
 <template>
-  <b-row class="page">
-      <b-col cols="12">
-        <h1 class="page--title"><span class="icon-calendar-check-o h4"></span> Feriados</h1>
-      </b-col>
-      <b-col cols="12">
-        <b-form>
-          <div class="panel panel-default">
-            <div class="panel-body">
-              <b-form-group id="dateHoliday"
-                            label="Ano:"
-                            label-for="year_holiday"
-                            label-class="col-md-1 control-label"
-                            class="row">
-              <b-form-select id="year_holiday"
-                            :options="years"
-                            v-model="form.year"
-                            required />
-              </b-form-group>
+  <b-col cols="12">
+    <b-row class="page">
+        <b-col cols="12">
+          <h1 class="page--title"><span class="icon-calendar-check-o h4"></span> Feriados</h1>
+        </b-col>
+        <b-col cols="6">
+          <b-form>
+            <div class="panel panel-default">
+              <div class="panel-body">
+                <b-form-group id="dateHoliday"
+                              label="Ano:"
+                              label-for="year_holiday"
+                              label-class="col-md-6 control-label pt-2"
+                              class="row">
+                <b-form-select id="year_holiday"
+                              :options="years"
+                              v-model="form.year"
+                              required />
+                </b-form-group>
+              </div>
+              <div class="panel-footer" style="background-color: #efefef;">
+                <b-button variant="secondary" @click.prevent="add()">Incluir</b-button>
+                <span class="separator"></span>
+                <b-button variant="primary" @click.prevent="doSearch()">Pesquisar</b-button>
+              </div>
             </div>
-          </div>
+          </b-form>
+        </b-col>
+    </b-row>
 
-          <div class="panel-footer">
-            <b-button variant="secondary" @click.prevent="add()">Incluir</b-button>
-            <b-button variant="primary" @click.prevent="doSearch()">Pesquisar</b-button>
-          </div>
-
-        </b-form>
+    <b-row>
+      <b-col cols="1"></b-col>
+      <b-col cols="4" class="col-md-offset-1">
+        <b-card no-body header="Janeiro">
+          <b-list-group flush>
+            <b-list-group-item class="flex-column align-items-start">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">01 <i>Segunda-feira</i></h5>
+                <h5>Nacional</h5>
+              </div>
+              <p class="mb-1">
+                Confraternização universal
+              </p>
+              <span><b>Não haverá expediente</b></span>
+            </b-list-group-item>
+          </b-list-group>
+        </b-card>
+        <b-card no-body header="Fevereiro">
+          <b-list-group flush>
+            <b-list-group-item class="flex-column align-items-start">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">12 <i>Segunda-feira</i></h5>
+                <h5>Não é feriado</h5>
+              </div>
+              <p class="mb-1">
+                <b>Descrição</b>: Carnaval
+              </p>
+              <span><b>Expediente</b>: Abono</span>
+            </b-list-group-item>
+            <b-list-group-item class="flex-column align-items-start">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">13 <i>Terça-feira</i></h5>
+                <h5>Não é feriado</h5>
+              </div>
+              <p class="mb-1">
+                <b>Descrição</b>: Carnaval
+              </p>
+              <span><b>Expediente</b>: Abono</span>
+            </b-list-group-item>
+          </b-list-group>
+        </b-card>
       </b-col>
-  </b-row>
+    </b-row>
+  </b-col>
 </template>
 
 <script>
@@ -40,7 +85,11 @@ export default {
       form: {
         year: new Date().getFullYear(),
       },
-      years: ['2017', '2018'],
+      years: ['2018', '2017', '2016'],
+      holidays: [
+        { id: '0', day: '01', month: 'Janeiro', dayOfWeek: 'Segunda-feira', classification: 'Nacional', description: 'Confraternização Universal', descriptionDayWork: 'Não haverá expediente' },
+        { id: '1', day: '12', month: 'Fevereiro', dayOfWeek: 'Segunda-feira', classification: 'Não é feriado', description: 'Carnaval', descriptionDayWork: 'Abono' },
+      ],
     };
   },
   methods: {
@@ -76,4 +125,25 @@ export default {
 .panel-body {
   padding: 15px;
 }
+
+.panel-footer {
+  padding: 10px 15px;
+  text-align: center;
+}
+
+.control-label {
+  text-align: right;
+  font-weight: bold;
+}
+
+.separator::after{
+  content: " ";
+  display: inline-block;
+  background: black;
+  margin: 0 10px;
+  height: 23px;
+  vertical-align: middle;
+  width: 1px;
+}
+
 </style>
