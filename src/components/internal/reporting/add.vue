d2dd3c2bd860741c6cd39b595e87798129a3384f
Glauber Batista <glauber.batista@pitang.com>
2018-09-12 15:57:31 -0300

    Disciplinas na reportagem

--- src/components/internal/reporting/add.vue
+++ src/components/internal/reporting/add.vue
@@ -4,17 +4,84 @@
     <b-modal ref="modal" centered title="Adicionar Reportagem" ok-title="Adicionar"
              cancel-title="Cancelar" @ok="save">
       <p v-if="period"><b>PERÍODO :</b> {{period.description | toUpper}}</p>
-      <b-form-select id="select" v-model="costCenter"class="mb-3" required >
+      <b-form-select id="select" v-model="costCenter" class="mb-3" required>
         <option v-for="cc in costCenters" :value="cc">{{cc.description | toUpper}}</option>
       </b-form-select>
-      <b-form-input
-        id="qtdHours"
-        type="number"
-        v-model="totalHoursCostCenter"
-        :min="0"
-        required
-        placeholder="Insira a quantidade de horas trabalhadas">
-      </b-form-input>
+
+      <b-form-group label="Tipo de reportagem">
+        <b-form-radio-group v-model="isPerDiscipline"
+                            :options="options"
+                            name="radios">
+        </b-form-radio-group>
+      </b-form-group>
+
+      <span v-show="isPerDiscipline">
+        <b-card-group deck style="padding-top: 10px !important;">
+          <b-card
+            header="Disciplinas"
+            header-tag="header">
+            <b-row>
+              <b-col cols="3"><label>REQ</label></b-col>
+              <b-col cols="9">
+                <b-form-input class="input-card"
+                  type="number"
+                  v-model="req"
+                  :min="0">
+                </b-form-input>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col cols="3"><label>A&P</label></b-col>
+              <b-col cols="9">
+                <b-form-input class="input-card"
+                  type="number"
+                  v-model="aep"
+                  :min="0">
+                </b-form-input>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col cols="3"><label>IMPLE</label></b-col>
+              <b-col cols="9">
+                <b-form-input class="input-card"
+                  type="number"
+                  v-model="imple"
+                  :min="0">
+                </b-form-input>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col cols="3"><label>TST</label></b-col>
+              <b-col cols="9">
+                <b-form-input class="input-card"
+                  type="number"
+                  v-model="tst"
+                  :min="0">
+                </b-form-input>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col cols="3"><label>P&G</label></b-col>
+              <b-col cols="9">
+                <b-form-input class="input-card"
+                  type="number"
+                  v-model="peg"
+                  :min="0">
+                </b-form-input>
+              </b-col>
+            </b-row>
+          </b-card>
+        </b-card-group>
+      </span>
+      <span v-show="!isPerDiscipline">
+          <b-form-input class="input-card"
+            type="number"
+            v-model="totalHoursCostCenter"
+            :min="0"
+            placeholder="Insira a quantidade de horas trabalhadas"
+            style="margin-top: 10px !important;">
+          </b-form-input>
+        </span>
     </b-modal>
   </div>
 </template>
@@ -32,10 +99,23 @@
     },
     data() {
       return {
+        isPerDiscipline: false,
+
+        req: 0,
+        aep: 0,
+        imple: 0,
+        tst: 0,
+        peg: 0,
+
         reporting: null,
         costCenter: null,
         costCenters: [],
-        totalHoursCostCenter: 0
+        totalHoursCostCenter: 0,
+
+        options : [
+          { text: 'Disciplina', value: true },
+          { text: 'Total de Horas', value: false },
+        ]
       };
     },
     mounted() {
@@ -57,12 +137,23 @@
           });
       },
       save(event) {
+        if (this.isPerDiscipline) {
+          this.totalHoursCostCenter = parseInt(this.req) + parseInt(this.aep) + parseInt(this.imple) + parseInt(this.tst) + parseInt(this.peg);
+        }
         this.$http()
           .post("reporting", {
             params: {
               user_id: this.user.ID,
               period: this.period,
               costCenter: this.costCenter,
+              isPerDiscipline: this.isPerDiscipline,
+              discipline: {
+                req: this.req,
+                aep: this.aep,
+                imple: this.imple,
+                tst: this.tst,
+                peg: this.peg,
+              },
               totalHoursCostCenter: this.totalHoursCostCenter
             }
           })
@@ -72,8 +163,13 @@
                 "Adicionado",
                 "Reportagem adicionada.",
                 "success",
+                this.req = 0,
+                this.aep = 0,
+                this.imple = 0,
+                this.tst = 0,
+                this.peg = 0,
                 this.costCenter = null,
-                this.totalHoursCostCenter = null,
+                this.totalHoursCostCenter = 0,
                 this.findUserCostCenterByUserIdWithoutReportingInPeriod(),
                 this.$emit("refresh"),
               );
@@ -83,6 +179,13 @@
                 "Erro",
                 "Erro ao inserir reportagem.",
                 "error",
+                this.req = 0,
+                this.aep = 0,
+                this.imple = 0,
+                this.tst = 0,
+                this.peg = 0,
+                this.costCenter = null,
+                this.totalHoursCostCenter = 0,
                 this.$emit("refresh")
               );
             }
@@ -91,11 +194,24 @@
       showModal() {
         this.findUserCostCenterByUserIdWithoutReportingInPeriod();
         this.$refs.modal.show();
-      }
+      },
     }, filters: {
       toUpper(value) {
         if (value !== "" && value !== undefined && value !== undefined)
           return value.toUpperCase();
+      }
+    }, watch: {
+      isPerDiscipline : function (newValue, oldValue) {
+        if (newValue) {
+          this.isPerDiscipline = true;
+        } else {
+          this.isPerDiscipline = false;
+          this.req = 0;
+          this.aep = 0;
+          this.imple = 0;
+          this.tst = 0;
+          this.peg = 0;
+        }
       }
     }
   };
@@ -105,8 +221,18 @@
     width: 100%;
     border-radius: 0cm;
   }
-  #select{
+
+  #select {
     width: 100%;
     border-radius: 0px;
+  }
+
+  label{
+    float: left;
+  }
+
+  .input-card {
+    width: 100% !important;
+    border-radius: 5px !important;
   }
 </style>
--- src/components/internal/reporting/edit.vue
+++ src/components/internal/reporting/edit.vue
@@ -6,15 +6,81 @@
       <p v-if="reporting.period.description"><b>PERÍODO : </b>{{reporting.period.description | toUpper}}</p>
       <label v-if="reporting.costCenter.description"><b>CENTRO DE CUSTO
         : </b>{{reporting.costCenter.description}}</label>
-      <b-form-input
-        id="qtdHours"
-        v-if="reporting.totalHoursCostCenter"
-        type="number"
-        v-model="reporting.totalHoursCostCenter"
-        required
-        :min="0"
-        placeholder="Quantidade de horas trabalhadas no CC">
-      </b-form-input>
+      <br /><br />
+      <b-form-group label="Tipo de reportagem" class="select-card">
+        <b-form-radio-group v-model="reporting.isPerDiscipline"
+                            :options="options"
+                            name="radios">
+        </b-form-radio-group>
+      </b-form-group>
+
+      <span v-show="reporting.isPerDiscipline">
+        <b-card-group deck style="padding-top: 10px !important;">
+          <b-card
+            header="Disciplinas"
+            header-tag="header">
+            <b-row>
+              <b-col cols="3"><label>REQ</label></b-col>
+              <b-col cols="9">
+                <b-form-input
+                  type="number"
+                  v-model="reporting.discipline.req"
+                  :min="0">
+                </b-form-input>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col cols="3"><label>A&P</label></b-col>
+              <b-col cols="9">
+                <b-form-input
+                  type="number"
+                  v-model="reporting.discipline.aep"
+                  :min="0">
+                </b-form-input>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col cols="3"><label>IMPLE</label></b-col>
+              <b-col cols="9">
+                <b-form-input
+                  type="number"
+                  v-model="reporting.discipline.imple"
+                  :min="0">
+                </b-form-input>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col cols="3"><label>TST</label></b-col>
+              <b-col cols="9">
+                <b-form-input
+                  type="number"
+                  v-model="reporting.discipline.tst"
+                  :min="0">
+                </b-form-input>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col cols="3"><label>P&G</label></b-col>
+              <b-col cols="9">
+                <b-form-input
+                  type="number"
+                  v-model="reporting.discipline.peg"
+                  :min="0">
+                </b-form-input>
+              </b-col>
+            </b-row>
+          </b-card>
+        </b-card-group>
+      </span>
+      <span v-show="!reporting.isPerDiscipline">
+        <b-form-input
+          type="number"
+          v-model="reporting.totalHoursCostCenter"
+          :min="0"
+          placeholder="Insira a quantidade de horas trabalhadas"
+          style="margin-top: 10px !important;">
+        </b-form-input>
+      </span>
     </b-modal>
   </div>
 </template>
@@ -29,6 +95,10 @@
     },
     data() {
       return {
+        options: [
+          {text: 'Disciplina', value: true},
+          {text: 'Total de Horas', value: false},
+        ]
       };
     },
     methods: {
@@ -36,6 +106,20 @@
         this.$refs.modal.show();
       },
       update() {
+        if (this.reporting.isPerDiscipline) {
+          this.reporting.totalHoursCostCenter =
+            parseInt(this.reporting.discipline.req) +
+            parseInt(this.reporting.discipline.aep) +
+            parseInt(this.reporting.discipline.imple) +
+            parseInt(this.reporting.discipline.tst) +
+            parseInt(this.reporting.discipline.peg);
+        }else{
+          this.reporting.discipline.req = 0;
+          this.reporting.discipline.aep = 0;
+          this.reporting.discipline.imple = 0;
+          this.reporting.discipline.tst = 0;
+          this.reporting.discipline.peg = 0;
+        }
         this.$http().put('reporting', this.reporting).then(() => {
           this.$swal(
             'Adicionado',
@@ -55,7 +139,7 @@
     },
     filters: {
       toUpper(value) {
-        if (value !== null && value !== undefined ){
+        if (value !== null && value !== undefined) {
           return value.toUpperCase();
         }
       }
@@ -76,9 +160,14 @@
     float: left;
   }
 
-  #qtdHours{
+  #qtdHours {
     width: 100%;
     border-radius: 0cm;
+  }
+
+  .select-card {
+    width: 100%;
+    text-align: left;
   }
 
 </style>
